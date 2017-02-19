66dae8e4b4f5fef01dbf046eea0a42c356aa108d
Shunsuke Watanabe <craftgear@gmail.com>
2017-02-19 20:36:00 +0900

    add react-native-camera module

--- index.ios.js
+++ index.ios.js
@@ -16,15 +16,8 @@ export default class CameraAppExpriment extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        Here are some changes!
         </Text>
       </View>
     );
